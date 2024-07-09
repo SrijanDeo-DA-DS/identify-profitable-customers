9bd171aa214aeb7f6b8ce1204452ed68c5317344
SrijanDeo-DA-DS <sd759@njit.edu>
2024-07-09 15:23:38 -0500

    initial commit with dvc config files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='THis project can help identify our profitable customers, visualize their spending habits, recommend similar customers based on spending and explain what factors contribute for profitable customers.',
+    author='SrijanDeo',
+    license='',
+)
